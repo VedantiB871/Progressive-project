e857324bc387859c3781978a7180f73f5943f200
username <wecp-dummy@gmail.com>
2024-07-26 12:44:03 +0000

    Day 19

--- frontend/src/app/bank/components/customerarray/customerarray.component.ts
+++ frontend/src/app/bank/components/customerarray/customerarray.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { of } from 'rxjs';
 import { CustomerTS } from '../../types/tstypes/Customerts';
-
+ 
 @Component({
   selector: 'app-customerarray',
-  standalone: true,
-  imports: [],
+  // standalone: true,
+  // imports: [],
   templateUrl: './customerarray.component.html',
   styleUrls: ['./customerarray.component.css']
 })
 export class CustomerarrayComponent {
-  
+  customers:CustomerTS[]=[
+    new CustomerTS('1','ryan','ryan@gmail.com','pass@123','ryan123','developer')
+  ];
 }
